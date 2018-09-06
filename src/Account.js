dc7be1067eced6b65964d69906806bd446e64896
Devan Grose <devangrose@gmail.com>
2018-09-05 19:09:04 -0700

    Finished homework, added flair

--- src/Account.js
+++ src/Account.js
@@ -5,23 +5,59 @@ export default class Account extends Component {
     super(props);
 
     this.state = {
-      balance: 0
-    }
+      balance: 0,
+      message: '',
+      messageClass:'hidden'
+    };
 
-    this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleDepositClick = this.handleDepositClick.bind(this);
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this);
   }
 
   handleDepositClick(e) {
     e.preventDefault();
-    if (isNaN(this.refs.amount.value)) {
+    if (isNaN(this.refs.amount.value) || parseInt(this.refs.amount.value) < 0) {
       console.log("Not a number");
+      this.setState({
+        message:'That is not a valid input!',
+        messageClass:''
+      });
     }
     else {
       let amount = +this.refs.amount.value;
       let newBalance = this.state.balance + amount;
       this.setState({
-        balance: newBalance
-      })
+        balance: newBalance,
+        message:'',
+        messageClass:'hidden'
+      });
+      this.refs.amount.value = '';
+    }
+  }
+  handleWithdrawClick(e) {
+    e.preventDefault();
+    if (isNaN(this.refs.amount.value) || parseInt(this.refs.amount.value) < 0) {
+      console.log("Not a number");
+      this.setState({
+        message:'That is not a valid input!',
+        messageClass:''
+      });
+    }
+    else if (parseInt(this.refs.amount.value) > this.state.balance) {
+      console.log("Too much!");
+      this.setState({
+        message:'Not enough funds!',
+        messageClass:''
+      });
+    }
+    else {
+      let amount = +this.refs.amount.value;
+      let newBalance = this.state.balance - amount;
+      this.setState({
+        balance: newBalance,
+        message:'',
+        messageClass:'hidden'
+      });
       this.refs.amount.value = '';
     }
   }
@@ -38,7 +74,12 @@ export default class Account extends Component {
         <div className={balanceClass}>${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref="amount" />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
+        <div>
+          <p class={this.state.messageClass}>{this.state.message}</p>
+        </div>
       </div>
+
     )
   }
 }
